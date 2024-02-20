c7581fc4730df289fcb2de8ce87ea0f779e7c515
lv <lv-178yandex.ru>
2024-02-20 14:04:37 +0300

    arithmetic class added

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,3 +1,45 @@
 public class Arithmetic {
 
+    private int firstNumber;
+    private int secondNumber;
+    private int result = 0;
+
+    public Arithmetic(int writeFirst, int writeSecond) {
+        firstNumber = writeFirst;
+        secondNumber = writeSecond;
+    }
+
+    public void sum() {
+        result = firstNumber + secondNumber;
+    }
+
+    public void sub() {
+        result = firstNumber - secondNumber;
+    }
+
+    public void min() {
+        if (firstNumber < secondNumber) {
+            result = firstNumber;
+        } else {
+            result = secondNumber;
+        }
+    }
+
+    public void max() {
+        if (firstNumber > secondNumber) {
+            result = firstNumber;
+        } else {
+            result = secondNumber;
+        }
+    }
+
+    public int getResult(String operation) {
+        switch (operation) {
+            case "+" -> sum();
+            case "-" -> sub();
+            case "min" -> min();
+            case "max" -> max();
+        }
+        return result;
+    }
 }
