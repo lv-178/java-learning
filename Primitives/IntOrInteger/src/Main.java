969fea7ec208c106417c2899fb3ef1b9e2d00299
alisa <lv-178yandex.ru>
2024-02-24 16:47:29 +0300

    error fixed, letter codes found

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -2,11 +2,16 @@ public class Main {
     public static void main(String[] args) {
         Container container = new Container();
         container.addCount(5672);
+        container.addCount(12);
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
+        for (int i=0; i<65536; i++) {
+            char symbol = (char) i;
+            if (alphabet.contains(String.valueOf(symbol))) {
+                System.out.println(symbol + " - " + i);
+            }
+        }
     }
 }
