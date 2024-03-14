ce878c45dc7782e0f69a8e4a9a7b81846b9152c6
alisa <lv-178yandex.ru>
2024-03-14 15:38:37 +0300

    airport done

--- StreamAPI/Airport/src/main/java/airport/Main.java
+++ StreamAPI/Airport/src/main/java/airport/Main.java
@@ -2,32 +2,68 @@ package airport;
 
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
+    public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
+        String model = "Boeing 737-800";
+
+        long numberOfAircraftsOfThisModel = findCountAircraftWithModelAirbus(airport, model);
+        System.out.println("Количество самолётов данной модели: " + numberOfAircraftsOfThisModel);
+
+        Map<String, Integer> parkedAircrafts = findMapCountParkedAircraftByTerminalName(airport);
+        System.out.println("Количество самолётов на каждом терминале: " + parkedAircrafts);
+
+        List<Flight> nextFlights = findFlightsLeavingInTheNextHours(airport, 3);
+        System.out.println("Количество вылетов в ближайшее время: " + nextFlights);
+
+        String terminal = "D";
+        System.out.println("Ближайший прилёт в терминал " + terminal + ": " +
+                findFirstFlightArriveToTerminal(airport, terminal));
+
+    }
 
     public static long findCountAircraftWithModelAirbus(Airport airport, String model) {
-        //TODO Метод должен вернуть количество самолетов указанной модели.
-        // подходят те самолеты, у которых name начинается со строки model
-        return 0;
+        return airport.getAllAircrafts().stream()
+                .filter(aircraft -> aircraft.toString().contains(model)).count();
     }
 
     public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
-        //TODO Метод должен вернуть словарь с количеством припаркованных самолетов в каждом терминале.
-        return Collections.emptyMap();
+        Map<String, Integer> terminals = new TreeMap<>();
+        airport.getTerminals().forEach(terminal -> terminals.put(terminal.getName(),
+                terminal.getParkedAircrafts().size()));
+        return terminals;
     }
 
     public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
-        //TODO Метод должен вернуть список отправляющихся рейсов в ближайшее количество часов.
-        return Collections.emptyList();
+        List<Flight> flights = new ArrayList<>();
+        airport.getTerminals().forEach(terminal -> terminal.getFlights()
+                        .forEach(flight -> {if ((flight.getDate()
+                                .isBefore(Instant.now()
+                                .plusSeconds(3600L * hours))) &
+                                        (flight.getDate().isAfter(Instant.now())) &
+                                (flight.getType() == Flight.Type.DEPARTURE)) {
+                            flights.add(flight);
+                        };}));
+        return flights;
     }
 
     public static Optional<Flight> findFirstFlightArriveToTerminal(Airport airport, String terminalName) {
-        //TODO Найти ближайший прилет в указанный терминал.
-        return Optional.empty();
+        Stream<Flight> stream = Stream.empty();
+        if (airport.getTerminals().stream().anyMatch(terminal -> terminal.getName().equals(terminalName))) {
+            stream = airport.getTerminals()
+                    .get(airport.getTerminals().indexOf((Terminal) (airport.getTerminals().stream()
+                            .filter(terminal -> terminal.getName().equals(terminalName))
+                            .findFirst().get())))
+                    .getFlights().stream().filter(flight -> (flight.getDate().isAfter(Instant.now())) &
+                            (flight.getType() == Flight.Type.ARRIVAL))
+                    .sorted(Comparator.comparing(Flight::getDate));
+        }
+        return stream.findFirst();
     }
 }
